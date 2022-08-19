9c14cde0e834d745089e7b5841e93ee2069fe8df
AAAxis <polskoydm@gmail.com>
2022-08-19 15:32:23 +0300

    1111

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ from config import mail_username, mail_password
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgresql@localhost/flasksql'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgresql@localhost/flasksql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'key'
 
